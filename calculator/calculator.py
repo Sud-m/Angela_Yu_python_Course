491cda05491cbf2cd7c5514b96c66a1577fbb3f1
Sud-m <smasti@asu.edu>
2024-01-07 16:42:39 -0700

    Work done till day 11

---
+++ calculator/calculator.py
@@ -0,0 +1,35 @@
+import art, os
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    return a / b
+operations={
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
+}
+def calculator():
+  os.system("cls")
+  print(art.logo)
+  num1=float(input("Enter the first number: "))
+  while True:
+    num2=float(input("Enter the next number: "))
+    for key in operations:
+      print(key) 
+    symbol=input("Pick an operation from the given symbols: ")
+    operation=operations[symbol]
+    
+    result=operation(num1,num2)
+    print(f"{num1} {symbol} {num2} = {result}")
+    cont=input(f"Type 'y' to continue calculating with {result}, or type 'n' to start over.: ").lower()
+    if not cont=='n':
+      num1=result
+    else:
+      calculator()
+
+calculator()
---
+++ hangman/hangman.py
@@ -0,0 +1,54 @@
+import random
+import hangman_words
+import hangman_art
+import os
+chosen_word = random.choice(hangman_words.word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+print(hangman_art.logo)
+print(f"Hint, word is {chosen_word}")
+#Create blanks
+display = ['_' for i in range(word_length)]
+guesses=[]
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    os.system("cls")
+    if guess in guesses:
+        print("You have already guessed that! You have lost a life. Please try again.")
+        lives-=1
+        print(f"{' '.join(display)}")
+        print(hangman_art.stages[lives])
+        if lives==0:
+            print("You lost")
+            print(f"Answer was: {chosen_word}")
+            end_of_game=True
+        continue
+    guesses+=guess
+    #Check guessed letter and replace display
+    for position in range(word_length):
+        letter = chosen_word[position]
+        
+        if letter == guess:
+            display[position] = letter
+
+    #Check if user is wrong.
+    if guess not in chosen_word:
+        lives -= 1
+    if lives == 0:
+        end_of_game = True
+        print("You lost.")
+        print(f"Answer was: {chosen_word}")
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"You guessed: {guess}")
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+        
+    print(hangman_art.stages[lives])
