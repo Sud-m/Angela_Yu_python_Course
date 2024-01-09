505606e13c8c5c343699d9569efc28516145c27c
Sud-m <smasti@asu.edu>
2024-01-09 16:16:24 -0700

    Pep 8 :)

--- cipher/cipher.py
+++ cipher/cipher.py
@@ -1,34 +1,37 @@
 from art import logo
 import os
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
 
 def caesar(start_text, shift_amount, cipher_direction):
-  """Either encode or decode given text."""
-  end_text = ""
-  if cipher_direction == "decode":
-    shift_amount *= -1
-  for char in start_text:
-    if char not in alphabet:
-      end_text+=char
-      continue
-    position = alphabet.index(char)
-    new_position = position + shift_amount
-    end_text += alphabet[new_position]
-    
-  print(f"Here's the {cipher_direction}d result: {end_text}")
+    """Either encode or decode given text."""
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char not in alphabet:
+            end_text += char
+            continue
+        position = alphabet.index(char)
+        new_position = position + shift_amount
+        end_text += alphabet[new_position]
 
- 
+    print(f"Here's the {cipher_direction}d result: {end_text}")
+
+
 while True:
-  os.system("cls")
-  print(logo)
-  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-  text = input("Type your message:\n").lower()
-  shift = int(input("Type the shift number:\n"))
-  
-
-  shift %= 26
-  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
-  if input("Type 'yes' if you want to go again. Otherwise type 'no'.").lower()=='no':
     os.system("cls")
-    print("Goodbye")
-    break
+    print(logo)
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift %= 26
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    if input("Type 'yes' if you want to go again. Otherwise type 'no'.").lower() == 'no':
+        os.system("cls")
+        print("Goodbye")
+        break
